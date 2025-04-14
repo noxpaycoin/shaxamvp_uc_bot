0c2f1fd90a6b8f61f1dcf9481fae960945ff9be6
noxpaycoin <winasror1@gmail.com>
2025-04-14 13:59:23 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+
+TOKEN = "7678852248:AAEBC-ZEpLv5t57MRaFh0L11fW1RiwXTyd4"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.reply_to(message, "Assalomu alaykum! SHAXAMVP UC BOT ga xush kelibsiz!")
+
+bot.infinity_polling()
